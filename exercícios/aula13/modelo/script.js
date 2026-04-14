8c1ea1b2598e14e88105a4891407845941f88511
Felpss14 <felipenestor1407@gmail.com>
2026-04-14 11:03:46 -0300

    adição de imagem e ajustes nas variáveis

--- exercícios/aula13/modelo/script.js
+++ exercícios/aula13/modelo/script.js
@@ -3,9 +3,25 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano) {
-        alert('Verifique os dados e tente novamente')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        alert('Tudo OK!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //Criança
+            } else if (idade >= 10 && idade < 21) {
+                //Jovem
+            }
+        } else if (fsex[1].checked) {
+            gênero = "Mulher"
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 }
