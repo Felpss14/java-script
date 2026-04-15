268cc77d01a61a8388e9552c5b602f9a460afac6
Felipe Nestor Colares da Nobrega <felipenestor1407@gmail.com>
2026-04-15 16:51:08 -0300

    contador que pula números

---
+++ exercícios/aula14/modelo/script.js
@@ -0,0 +1,36 @@
+function contar() {
+    var ini = document.getElementById('txti').value
+    var fim = document.getElementById('txtf').value
+    var passo = document.getElementById('txtp').value
+    var res = document.getElementById('res')
+
+    if (ini.length == 0 || fim.length == 0) {
+        res.innerHTML = "Impossível contar!"
+        return
+    }
+
+    if (passo.length == 0 || passo == 0) {
+        alert("Vamos considerar o passo como 1")
+        passo = 1
+    }
+
+    res.innerHTML = 'Contando: '
+
+    var i = Number(ini)
+    var f = Number(fim)
+    var p = Number(passo)
+
+    if (i < f) {
+        // Contagem crescente
+        for (var c = i; c <= f; c += p) {
+            res.innerHTML += `${c} 👉 `
+        }
+    } else {
+        // Contagem regressiva
+        for (var c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} 👉 `
+        }
+    }
+
+    res.innerHTML += `🏁`
+}
