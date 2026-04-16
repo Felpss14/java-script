4e32aab9fb1d5926af793b0e1ec5bdcf68c41446
Felpss14 <felipenestor1407@gmail.com>
2026-04-16 08:35:22 -0300

    Criação do exercicio de tabuada

---
+++ exercícios/aula14-b/modelo/script.js
@@ -0,0 +1,7 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {}
+    let n = Number(num.value)
+
+}
